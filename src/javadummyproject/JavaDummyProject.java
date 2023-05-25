55491ab863a2409696186b9186b201f038117b87
Mharoon0522 <mharoon0522@gmail.com>
2023-05-25 14:31:19 -0400

    Version 1

---
+++ src/javadummyproject/JavaDummyProject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javadummyproject;
+
+
+public class JavaDummyProject {
+
+    /*push steps: 1. initialize versioning, .gitconfig file, .gitignore,Local repository,
+      commit */
+    public static void main(String[] args) {
+      
+       System.out.println("Hello World");
+    }
+    
+}
